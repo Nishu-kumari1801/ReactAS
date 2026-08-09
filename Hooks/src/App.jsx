8cf78121cd0ea6284c9cc4e076d54cbe27d59451
Nishu Kumari <nishujha.nj01@gmail.com>
2026-08-09 14:02:38 +0530

    Updating objects in state

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -643,12 +643,53 @@ import DisplayUser from './DisplayUser';
 //                 lecture :-17 (Lifting State Up)
 // ======================================================
 
+// function App(){
+//    const [user,setUser] = useState('')
+//   return (
+//     <div>
+//       <AddUser setUser={setUser}/>
+//       <DisplayUser user={user}/>
+//     </div>
+//   )
+// }
+
+// ======================================================
+//                 lecture :-18 (Updating objects in state)
+// ======================================================
+
 function App(){
-   const [user,setUser] = useState('')
+  // const [name,setName] = useState('nishu');
+
+  const [data,setData]=useState({
+    name:'nishu',
+    address:{
+      city:'delhi',
+      country:'India'
+    }
+  })
+
+  const handleName=(val)=>{
+    data.name=val;
+    console.log(data);
+    setData({...data});
+  }
+
+  const handleCity=(city)=>{
+    data.address.city=city;
+    console.log(city);
+    setData({...data,address:{...data.address,city}})
+  }
+
   return (
     <div>
-      <AddUser setUser={setUser}/>
-      <DisplayUser user={user}/>
+      <h1>Updating objects in state</h1>
+      
+      {/* <button onClick={handleName}>Update name</button> */}
+      <input type="text" placeholder="update name" onChange={(event)=>handleName(event.target.value)}/>
+      <input type="text" placeholder="update city" onChange={(event)=>handleCity(event.target.value)}/>
+      <h2>Name: {data.name}</h2>
+      <h2>City: {data.address.city}</h2>
+      <h2>Country: {data.address.country}</h2>
     </div>
   )
 }
