0e6b3ec70faa7b9a3920a7eddeb5da13c1592fbb
Nishu Kumari <nishujha.nj01@gmail.com>
2026-08-11 17:48:37 +0530

    Integrate API for DELETE Method

--- API/src/UserList.jsx
+++ API/src/UserList.jsx
@@ -10,14 +10,26 @@ function UserList(){
     setLoading(true);
     getUserData();
   },[])
+  const url = "http://localhost:3000/users";
 
   const getUserData = async()=>{
-    const url = "http://localhost:3000/users";
     let response = await fetch(url);
     response=await response.json();
     console.log(response);
     setUserData(response);
     setLoading(false);
+  }
+
+  const deleteUser=async(id)=>{
+    let response = await fetch(url+"/"+id,{
+        method:'delete'
+    });
+    response = await response.json();
+    if(response){
+        alert("record deleted");
+        getUserData();
+    }
+    console.log(id);
   }
   return (
     <>
@@ -25,6 +37,7 @@ function UserList(){
           <li>Name</li>
           <li>Age</li>
           <li>Email</li>
+          <li>Action</li>
       </ul>
     {
       !loading?
@@ -33,6 +46,7 @@ function UserList(){
           <li>{user.name}</li>
           <li>{user.age}</li>
           <li>{user.email}</li>
+          <li><button onClick={()=>deleteUser(user.id)}>Delete</button></li>
          </ul>
       ))
       :<h1>Data Loading....</h1>
