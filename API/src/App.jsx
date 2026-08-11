50f6fd3a22f3cf981324f25b20109af8c4a78533
Nishu Kumari <nishujha.nj01@gmail.com>
2026-08-11 11:51:43 +0530

    learning API

---
+++ API/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+// ======================================================
+//                 lecture :-01 (What is API)
+// ======================================================
+
+/**
+ * Application programming interface
+ * we need data from db when making  projects
+ * but js can not connect with database as js is a clent site scripting language and 
+ * database is server side scripting language.
+ * so we make api in other language as java php or node etc
+ */
+
+/** Shared data with API
+ * we need some data in multiple platform
+ * like web app , mobile app , windows OS etc
+ * so we make API in one language
+ * And use same API with all platforms
+ * Data format :- API uses JSON data
+ */
+
+// function App() {
+//   return (
+//     <>
+//      <h1>What is API</h1>
+//     </>
+//   )
+// }
+
+
+// ======================================================
+//                 lecture :-02 (Get Data from GET API Method in React)
+// ======================================================
+
+function App() {
+  return (
+    <>
+     <h1>Get Data from GET API Method in React</h1>
+    </>
+  )
+}
+
+
+
+export default App
